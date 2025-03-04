a70a37a806578dbb48e924fb4f7ff577454e0314
samcharles5 <sc240843@gmail.com>
2025-03-04 15:46:17 +0530

    portfolio link updated

--- contact-management-frontend/src/App.js
+++ contact-management-frontend/src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
 import Contacts from './Contacts';
 
 function App() {
-  return (
+  return (<>
    <Contacts />
+   <div className="position-fixed top-0 end-0 translate-middle-x p-3 anker m-5"> <b>Check out my portfolio <a href="https://samcharles-sde.web.app/">Link</a></b></div>
+   </>
   );
 }
 
